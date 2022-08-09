e58c68cd0bf77ecfdc547942577671c3d831e05d
Facundo <facu03@gmail.com>
2022-08-09 19:47:43 -0300

    puede que la haya cagado

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/SNAPSHOT-0.0.1.jar
--- src/main/java/com/ejemplo/springboot/SpringbootApplication.java
+++ src/main/java/com/ejemplo/springboot/SpringbootApplication.java
@@ -2,7 +2,9 @@ package com.ejemplo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @SpringBootApplication
 public class SpringbootApplication {
 
@@ -10,19 +12,5 @@ public class SpringbootApplication {
 		SpringApplication.run(SpringbootApplication.class, args);
                 
 	}
-//        @Bean
-//	public CorsFilter corsFilter() {
-//		CorsConfiguration corsConfiguration = new CorsConfiguration();
-//		corsConfiguration.setAllowCredentials(true);
-//		corsConfiguration.setAllowedOrigins(Arrays.asList("https://https://porfolio-cf060.web.app/"));
-//		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-//				"Accept", "Authorization", "Origin, Accept", "X-Request-With",
-//				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-//		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-type", "Accept", "Authorization",
-//				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-//		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE", "OPTIONS"));
-//		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-//		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-//		return new CorsFilter(urlBasedCorsConfigurationSource);
-//	}
+
 }
