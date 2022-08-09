6eb51072a445d7430449a62434c3f24543ba0997
Facundo <facu03@gmail.com>
2022-08-09 19:55:56 -0300

    la arreglo?

--- src/main/java/com/ejemplo/springboot/SpringbootApplication.java
+++ src/main/java/com/ejemplo/springboot/SpringbootApplication.java
@@ -2,15 +2,12 @@ package com.ejemplo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 
-@Controller
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
-                
 	}
 
 }
--- src/main/java/com/ejemplo/springboot/security/repository/IRolRepository.java
+++ src/main/java/com/ejemplo/springboot/security/repository/IRolRepository.java
@@ -10,5 +10,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IRolRepository extends JpaRepository<Rol, Integer>{
     Optional<Rol> findByRolNombre(RolNombre rolNombre);
-
 }
